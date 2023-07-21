bb74fab6342cdc619106b5d574873ffc576f13cd
ChrolloL <batistalucas451@gmail.com>
2023-07-21 15:07:39 -0300

    alterações

--- src/core/settings.py
+++ src/core/settings.py
@@ -4,6 +4,8 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+GEOS_LIBRARY_PATH = os.path.join(BASE_DIR, "venv","Lib","site-packages","osgeo", "geos_c.dll")  
+GDAL_LIBRARY_PATH = os.path.join(BASE_DIR, "venv","Lib","site-packages","osgeo", "gdal304.dll")  
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -25,6 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'leaflet',
     'mapas',
 ]
 
